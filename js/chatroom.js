cef18bc4d7551984ec3d2a637cc9b0b90c900dc2
StudentCode09 <jlopez092494@gmail.com>
2017-06-13 15:25:38 -0600

    SECOND COMMIT CHAT

--- js/chatroom.js
+++ js/chatroom.js
@@ -36,3 +36,62 @@ function openMail(personName) {
     document.getElementById(personName).style.display = "block";
     event.currentTarget.className += " w3-light-grey";
 }
+
+var app = angular.module("app", []);
+
+var contacts = [{
+		username: "Jake",
+		img: "http://s13.postimg.org/ih41k9tqr/img1.jpg",
+    status: "online"
+	},{
+	  username: "Wolf",
+		img: "http://s3.postimg.org/yf86x7z1r/img2.jpg",
+    status: "offline"
+	},{
+		username: "Weird Guy",
+		img: "http://s3.postimg.org/h9q4sm433/img3.jpg",
+    status: "online"
+	},{
+		username: "Drake",
+		img: "http://s30.postimg.org/kwi7e42rh/img6.jpg",
+    status: "offline"
+	}];
+
+var chats=[{
+		name: "Room 1"
+	},{
+		name: "Room 2"
+	},{
+		name: "Room 0"
+	},{
+		name: "Room 5"
+	}];
+
+  var currentUser=[{
+  		name: "Jorge",
+      username: "jlopez",
+      img: "src"
+  	}];
+
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+
+function addRoom(){
+	var newRoom = document.getElementById("nameRoom").value
+	console.log(rooms[0].name)
+	rooms[0].name = "G"
+	rooms.push({name: newRoom})
+}
+
+app.controller("ListController", ["$scope",function($scope){
+	$scope.users = users;
+
+	$scope.rooms = rooms;
+}]);
